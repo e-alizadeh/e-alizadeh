b41ba658a72444dd763023ea3ffc50fa96024a06
Essi <no-reply@ealizadeh.com>
2023-05-15 22:36:49 -0400

    fix: :ambulance: fix parsing issue after website migration

--- src/update_latest_blog_posts.py
+++ src/update_latest_blog_posts.py
@@ -7,11 +7,9 @@ from pathlib import Path
 import datetime
 import pytz
 from itertools import chain
-import requests
-from bs4 import BeautifulSoup
+import feedparser
 
-MAIN_WEBSITE = "https://ealizadeh.com"
-BLOG_FEED = f"{MAIN_WEBSITE}/blog"
+RSS_FEED_URL = "https://ealizadeh.com/index.xml"
 NUM_POST = 7
 READ_MORE_BADGE_URL = "https://img.shields.io/badge/-Read%20more%20on%20my%20blog-brightgreen?style=for-the-badge"
 READ_MORE_BADGE_HTML = f'<a href="https://ealizadeh.com/blog" target="_blank"><img alt="Personal Blog" src="{READ_MORE_BADGE_URL}" /></a>'
@@ -29,19 +27,18 @@ def flatten_post_tags(post_tags):
 
 
 def update_latest_blog_posts_readme(blog_feed, readme_base, join_on):
-    post_tags = blog_feed.find_all("a")
-
-    posts = []
-    for count, post in enumerate(post_tags):
-        post_url = MAIN_WEBSITE + post.get("href")
-        post_content = BeautifulSoup(requests.get(post_url).text, "lxml")
-        post_title = post_content.find('h1').text
-        posts.append(f' - [{post_title}]({post_url})')
+    blog_posts = blog_feed['entries']
+    posts_to_add = []
+    for count, post in enumerate(blog_posts):
+        # remove the unnecessary 'index.html' at the end of the blog url https://ealizadeh.com/blog/abc/index.html
+        post_url = post["link"].rstrip("index.html")
+        post_title = post["title"]
+        posts_to_add.append(f' - [{post_title}]({post_url})')
 
         if count == NUM_POST:
             break
 
-    posts_joined = "\n".join(posts)
+    posts_joined = "\n".join(posts_to_add)
     return readme_base[:readme_base.find(rss_title)] + f"{join_on}\n{posts_joined}"
 
 
@@ -49,11 +46,9 @@ if __name__ == "__main__":
     rss_title = "## 📕 Latest Blog Posts"
     readme = Path("../README.md").read_text(encoding="utf8")
 
-    html_content = requests.get(BLOG_FEED).text
-    soup = BeautifulSoup(html_content, "lxml")
-    main_blog_content = soup.find("main")
+    blog_feed = feedparser.parse(RSS_FEED_URL)
 
-    updated_readme = update_latest_blog_posts_readme(main_blog_content, readme, rss_title)
+    updated_readme = update_latest_blog_posts_readme(blog_feed, readme, rss_title)
     updated_readme += f"\n<space>\n \t {READ_MORE_BADGE_HTML}\n"
 
     with open("../README.md", "w+") as f:
